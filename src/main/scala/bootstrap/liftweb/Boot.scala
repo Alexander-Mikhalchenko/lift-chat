3ba0494e6bc4ce9fa4ce7648ac7e9fa08dce6996
Alexander Mikhalchenko <alex_gmr@inet.ua>
2014-08-29 18:58:42 +0300

    Remove redundant code from Boot.scala.

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -2,10 +2,8 @@ package bootstrap.liftweb
 
 import net.liftweb._
 
-import common._
 import http._
 import sitemap._
-import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
 
@@ -20,31 +18,18 @@ class Boot {
 
     // Build SiteMap
     val entries = List(
-      Menu.i("Home") / "index", // the simple way to declare a menu
-
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
-        "Static Content")))
+      Menu.i("Home") / "index") // the simple way to declare a menu
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
-
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery191
+    JQueryModule.InitParam.JQuery = JQueryModule.JQuery191
     JQueryModule.init()
 
   }
